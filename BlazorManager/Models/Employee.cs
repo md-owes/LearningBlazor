e31263fa2cb4153be4b9936150997baee041a1c4
Mohammed Owes <mohammed.owes@rsa.com>
2021-05-27 18:01:47 +0530

    added employee list page

--- BlazorManager/Models/Employee.cs
+++ BlazorManager/Models/Employee.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BlazorManager.Models
 {
@@ -12,5 +14,61 @@ namespace BlazorManager.Models
         public GenderEnum Gender { get; set; }
         public Department DepartmentDetails { get; set; }
         public string PhotoLocation { get; set; }
+
+        public static async Task<IEnumerable<Employee>> FetchAllEmployees()
+        {
+            Task delayTask = Task.Delay(3000);
+            Employee emp1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Sameer",
+                LastName = "Khan",
+                Email = "sameer@abc.com",
+                DateOfBirth = new DateTime(1985, 11, 25),
+                DepartmentDetails = new Department { DepartmentId = 1, DepartmentName = "HR" },
+                Gender = GenderEnum.Male,
+                PhotoLocation = "images/sameer.jpg"
+            };
+
+            Employee emp2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Falak",
+                LastName = "Naaz",
+                Email = "falak@abc.com",
+                DateOfBirth = new DateTime(1991, 08, 03),
+                DepartmentDetails = new Department { DepartmentId = 1, DepartmentName = "HR" },
+                Gender = GenderEnum.Female,
+                PhotoLocation = "images/falak.jpg"
+            };
+
+            Employee emp3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Tanveer",
+                LastName = "Ahmed",
+                Email = "tanveer@abc.com",
+                DateOfBirth = new DateTime(1989, 05, 14),
+                DepartmentDetails = new Department { DepartmentId = 2, DepartmentName = "Engineering" },
+                Gender = GenderEnum.Male,
+                PhotoLocation = "images/tanveer.jpg"
+            };
+
+            Employee emp4 = new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "Aira",
+                LastName = "Khatun",
+                Email = "aira@abc.com",
+                DateOfBirth = new DateTime(1993, 02, 19),
+                DepartmentDetails = new Department { DepartmentId = 3, DepartmentName = "Finance" },
+                Gender = GenderEnum.Female,
+                PhotoLocation = "images/aira.jpg"
+            };
+
+            await delayTask;
+
+            return new List<Employee> { emp1, emp2, emp3, emp4 };
+        }
     }
 }
---
+++ BlazorServer/Pages/EmployeeList.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorManager.Models;
+
+namespace BlazorServer.Pages
+{
+    public partial class EmployeeList
+    {
+        public IEnumerable<Employee> lstEmployees { get; set; }
+
+        public EmployeeList()
+        {
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            lstEmployees = await Employee.FetchAllEmployees();
+        }
+    }
+}
